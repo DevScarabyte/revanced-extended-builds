9ed231513de52ba24099a59c51b61615f3d89b55
E85Addict <77761710+E85Addict@users.noreply.github.com>
2023-02-14 16:52:01 -0800

    Sync with upstream

--- build-termux.sh
+++ build-termux.sh
@@ -24,7 +24,7 @@ yes "" | pkg update -y && pkg install -y git wget openssl jq openjdk-17 zip
 
 pr "Cloning revanced-extended-builds repository..."
 if [ -d revanced-extended-builds ]; then
-	if ask "Directory revanced-extended-builds already exists. Do you want to clone the repo again? [y/n]"; then
+	if ask "Directory revanced-extended-builds already exists. Do you want to clone the repo again and overwrite your config? [y/n]"; then
 		rm -rf revanced-extended-builds
 		git clone https://github.com/E85Addict/revanced-extended-builds --recurse --depth 1
 		sed -i '/^enabled.*/d; /^\[.*\]/a enabled = false' revanced-extended-builds/config.toml
@@ -33,7 +33,10 @@ else
 	git clone https://github.com/E85Addict/revanced-extended-builds --recurse --depth 1
 	sed -i '/^enabled.*/d; /^\[.*\]/a enabled = false' revanced-extended-builds/config.toml
 fi
-cd revanced-extended-builds
+
+if [ ! -f build.sh ]; then
+	cd revanced-extended-builds
+fi
 
 if ask "Do you want to open the config.toml for customizations? [y/n]"; then
 	nano config.toml
@@ -47,8 +50,10 @@ fi
 
 cd build
 pr "Ask for storage permission"
-until ls /sdcard >/dev/null 2>&1; do
+until
 	yes | termux-setup-storage >/dev/null 2>&1
+	ls /sdcard >/dev/null 2>&1
+do
 	sleep 1
 done
 
@@ -59,7 +64,7 @@ for op in *; do
 	cp -f "${PWD}/${op}" ~/storage/downloads/revanced-extended-builds/"${op}"
 done
 
-pr "Outputs are available in /sdcard/Download folder"
+pr "Outputs are available in /sdcard/Download/revanced-extended-builds folder"
 am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-extended-builds -t resource/folder
 sleep 2
 am start -a android.intent.action.VIEW -d file:///sdcard/Download/revanced-extended-builds -t resource/folder
